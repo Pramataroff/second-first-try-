30d8a6b2492ad5e4c4a2961e15e59ab57f095af0
pramataroff <milenpramatarov@gmail.com>
2024-02-16 12:13:34 +0200

    second first try

---
+++ Druj kradeca/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.ComponentModel.Design;
+using System.Runtime.InteropServices.Marshalling;
+
+namespace Druj_kradeca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter type: long, int or sbyte ");
+            string type = Console.ReadLine();
+            Console.Write("Enter how many ID's");
+            int nnum = int.Parse(Console.ReadLine());
+            int id = 0;
+            int difference = 0;
+            int difference1 = 0;
+            if (type == "sbyte")
+                for (int i = 1; i <= nnum; i++)
+                {
+                    id = int.Parse(Console.ReadLine());
+
+                    if (id < 128 && id > -129)
+                    {
+                        Console.WriteLine($"number {id} is in sbyte diapason");
+
+                    }
+
+
+                }
+            else if (type == "int")
+                for (int i = 1; i <= nnum; i++)
+                {
+                    id = int.Parse(Console.ReadLine());
+
+                    if (id > -2147483649 && id < 2147483647)
+                    {
+                        Console.WriteLine($"number {id} is in int diapason");
+
+                    }
+
+
+                }
+           else if (type == "long")
+                for (int i = 1; i <= nnum; i++)
+                {
+                    id = int.Parse(Console.ReadLine());
+
+                    if ((long)id < 2147483647 && (long)id > -2147483649)
+                    {
+                        Console.WriteLine($"number {(long)id} is in long diapason");
+
+                    }
+
+
+                }
+        }
+    }
+}
